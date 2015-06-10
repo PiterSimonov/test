69c0925709684ab6140ad4064b5e417d03fe4b8e
Antimafia <famia@ukr.net>
2015-06-10 23:59:22 +0300

    change some issues
    change some issues

--- src/test/java/com/codenvy/testtask/qname/QNameParserTest.java
+++ src/test/java/com/codenvy/testtask/qname/QNameParserTest.java
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 package com.codenvy.testtask.qname;
 
-=======
-package test.java.com.codenvy.testtask.qname;
-
-import main.java.com.codenvy.testtask.qname.IllegalNameException;
-import main.java.com.codenvy.testtask.qname.QName;
-import main.java.com.codenvy.testtask.qname.QNameParser;
->>>>>>> 1e01a236f7c36a898b87878c772293e9cb314c04
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -44,33 +36,20 @@ public class QNameParserTest extends Assert {
         QName qName = qNameParser.parse("_Pref-ix:someName");
         assertEquals("someName", qName.getLocalName());
         assertEquals("_Pref-ix", qName.getPrefix());
-<<<<<<< HEAD
         assertEquals("_Pref-ix:someName", qName.getAsString());
     }
 
     @Test(expected = IllegalNameException.class)
     public void testNullNameFailed() throws IllegalNameException {
-=======
-        assertEquals("_Pref-ix:some Name", qName.getAsString());
-    }
-
-    @Test(expected = IllegalNameException.class)
-    public void testNullNameFault() throws IllegalNameException {
->>>>>>> 1e01a236f7c36a898b87878c772293e9cb314c04
         qNameParser.parse(null);
     }
 
     @Test(expected = IllegalNameException.class)
-<<<<<<< HEAD
     public void testNoNameFailed() throws IllegalNameException {
-=======
-    public void testNoNameFault() throws IllegalNameException {
->>>>>>> 1e01a236f7c36a898b87878c772293e9cb314c04
         qNameParser.parse("");
     }
 
     @Test(expected = IllegalNameException.class)
-<<<<<<< HEAD
     public void testColonWithoutPrefixFailed() throws IllegalNameException {
         qNameParser.parse(":name");
     }
@@ -129,22 +108,5 @@ public class QNameParserTest extends Assert {
     public void testPrefixedNameWithColonFailed() throws IllegalNameException {
         qNameParser.parse("prefix:other:Name");
     }
-=======
-    public void testColonWithoutPrefixFault() throws IllegalNameException {
-        qNameParser.parse(":name");
-    }
-//
-//    .
-//    ..
-//    prefix:
-//    <пробел>name
-//    <пробел>prefix:name
-//    prefix: <пробел>name
-//    prefix:name<пробел>
-//    pre fix:name
-//    name/name
-//    name[name
-//    prefix:name:name
->>>>>>> 1e01a236f7c36a898b87878c772293e9cb314c04
 
 }
