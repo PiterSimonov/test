4737628ffb57c96af13d344b4d8f9fdfb01da234
Antimafia <famia@ukr.net>
2015-06-12 00:00:25 +0300

    change rexex compiling

--- src/main/java/com/codenvy/testtask/qname/QNameParser.java
+++ src/main/java/com/codenvy/testtask/qname/QNameParser.java
@@ -4,19 +4,22 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class QNameParser {
-    private final String LOCAL_NAME_REGEX = "\\A([\\p{ASCII}&&[^/:'\\u0022\\s\\Q[]*|\\E]]+\\u0020?" +
+    private static final String LOCAL_NAME_REGEX = "\\A([\\p{ASCII}&&[^/:'\\u0022\\s\\Q[]*|\\E]]+\\u0020?" +
             "[\\p{ASCII}&&[^/:'\\u0022\\s\\Q[]*|\\E]]+)+(?!\\u0020)\\z";
-    private final String SIMPLE_NAME_REGEX = "\\A(\\.?[\\p{ASCII}&&[^/:'\\u0022\\s\\Q.[]*|\\E]]+|" +
+    private static final String SIMPLE_NAME_REGEX = "\\A(\\.?[\\p{ASCII}&&[^/:'\\u0022\\s\\Q.[]*|\\E]]+|" +
             "[\\p{ASCII}&&[^/:'\\u0022\\s\\Q.[]*|\\E]]+\\.?|" +
             "([\\p{ASCII}&&[^/:'\\u0022\\s\\Q.[]*|\\E]]+\\u0020?" +
             "[\\p{ASCII}&&[^/:'\\u0022\\s\\Q.[]*|\\E]]+)+)(?!\\u0020)\\z";
-    private final String PREFIX_REGEX = "\\A(?!((X|x)(M|m)(L|l)))[\\p{Alpha}_]+[\\p{Alnum}\\u002E_-]*$";
+    private static final String PREFIX_REGEX = "\\A(?!((X|x)(M|m)(L|l)))[\\p{Alpha}_]+[\\p{Alnum}\\u002E_-]*$";
+    private static final Pattern LOCAL_NAME_PATTERN = Pattern.compile(LOCAL_NAME_REGEX);
+    private static final Pattern SIMPLE_NAME_PATTERN = Pattern.compile(SIMPLE_NAME_REGEX);
+    private static final Pattern PREFIX_PATTERN = Pattern.compile(PREFIX_REGEX);
 
     public QName parse(String name) throws IllegalNameException {
         String prefix = null;
         String qName;
         
-        if ((name == null) || (name.equals(""))) { 
+        if ((name == null) || (name.equals(""))) {
             throw new IllegalNameException("empty name");
         }
         if (name.contains(":")) {
@@ -24,27 +27,26 @@ public class QNameParser {
             if (result.length != 2) {
                 throw new IllegalNameException("invalid name");
             }
-            if ((result[0].length() != 0) 
-                  && (isValidName(result[0], PREFIX_REGEX))
-                  && (isValidName(result[1], LOCAL_NAME_REGEX))) {
+            if ((result[0].length() != 0)
+                    && (isValidName(result[0], PREFIX_PATTERN))
+                    && (isValidName(result[1], LOCAL_NAME_PATTERN))) {
                 prefix = result[0];
                 qName = result[1];
             } else {
                 throw new IllegalNameException("invalid name");
             }
-        } else if (isValidName(name, SIMPLE_NAME_REGEX)){ 
+        } else if (isValidName(name, SIMPLE_NAME_PATTERN)) {
             qName = name;
         } else {
             throw new IllegalNameException("invalid name");
         }
-        
+
         return new QName(prefix, qName);
     }
 
-    private boolean isValidName(String name, String regex) {
-        Pattern pattern = Pattern.compile(regex);
+    private boolean isValidName(String name, Pattern pattern) {
         Matcher matcher = pattern.matcher(name);
         return matcher.matches();
     }
-
+    
 }
